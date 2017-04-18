3ac4082a1edc8e4257020075a5e83084cc3d88aa
ThomasLengeling <thomas@codigogenerativo.com>
2017-04-18 14:56:19 -0400

    files

--- video_all.py
+++ video_all.py
@@ -1,7 +1,9 @@
 import os
 import math
 
-DIR = local_frame_dir
+cwd = os.getcwd()
+
+DIR = cwd+'/images/movie_charles_in'
 numImgs = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 
 print(numImgs)
