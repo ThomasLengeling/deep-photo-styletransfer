bb3ee4a5462e544964ae641dd783f8ea32ebbe0e
ThomasLengeling <thomas@codigogenerativo.com>
2017-04-18 14:59:57 -0400

    index

--- video_all.py
+++ video_all.py
@@ -9,9 +9,11 @@ numImgs = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(D
 print(numImgs)
 
 for i in range(numImgs):
-    cmd = ''
 
-    'python3 deep_photo.py -content_image examples/movie_charles_in/frame'+i+'.png -content_seg examples/movie_charles_seg/frame'+i+'.png -style_image examples/city_night.png -style_seg examples/city_night_seg.png -laplacian examples/movie_charles_lap/frame_'+i+'_700.csv -output_image examples/movie_charles_out/frame'+i+'.png -image_size 700'
+    cmd = ''
+    index = str(i)
+
+    'python3 deep_photo.py -content_image examples/movie_charles_in/frame'+index+'.png -content_seg examples/movie_charles_seg/frame'+index+'.png -style_image examples/city_night.png -style_seg examples/city_night_seg.png -laplacian examples/movie_charles_lap/frame_'+index+'_700.csv -output_image examples/movie_charles_out/frame'+index+'.png -image_size 700'
 
     print(cmd)
     os.system(cmd)
