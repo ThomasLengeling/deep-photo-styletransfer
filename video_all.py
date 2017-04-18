8930deba5bef7c0fe9abc038bbdd98e89a685704
ThomasLengeling <thomas@codigogenerativo.com>
2017-04-18 14:58:54 -0400

    range

--- video_all.py
+++ video_all.py
@@ -8,7 +8,7 @@ numImgs = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(D
 
 print(numImgs)
 
-for i in rang(numImgs):
+for i in range(numImgs):
     cmd = ''
 
     'python3 deep_photo.py -content_image examples/movie_charles_in/frame'+i+'.png -content_seg examples/movie_charles_seg/frame'+i+'.png -style_image examples/city_night.png -style_seg examples/city_night_seg.png -laplacian examples/movie_charles_lap/frame_'+i+'_700.csv -output_image examples/movie_charles_out/frame'+i+'.png -image_size 700'
